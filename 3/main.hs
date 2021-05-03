b3dfb20ea5d672bd472e4b0f680df69b395ee2ff
Jacob Men <jacobmen416@yahoo.com>
2021-05-03 15:18:21 -0500

    problem 3

---
+++ 3/main.hs
@@ -0,0 +1,17 @@
+isqrt :: (Integral a) => a -> Float
+isqrt n = sqrt $ fromIntegral n
+
+primeFactors' :: (Integral a) => a -> a -> [a]
+primeFactors' n divisor
+    | even n            = 2 : primeFactors' (n `div` 2) divisor
+    | divisor <= floor (isqrt n) = if n `rem` divisor == 0
+                             then divisor : primeFactors' (n `div` divisor) divisor
+                          else primeFactors' n (divisor + 2)
+    | n > 2             = [n]
+    | n <= 2             = []
+
+primeFactors :: (Integral a) => a -> [a]
+primeFactors n = primeFactors' n 3
+
+main = do
+    print $ last $ primeFactors 600851475143
