d8fd499979b44a4aa75cb4e7bb81a56ba45d9ab8
Jacob Men <jacobmen416@yahoo.com>
2021-05-03 15:21:35 -0500

    cleanup

--- 3/main.hs
+++ 3/main.hs
@@ -3,12 +3,12 @@ isqrt n = sqrt $ fromIntegral n
 
 primeFactors' :: (Integral a) => a -> a -> [a]
 primeFactors' n divisor
-    | even n            = 2 : primeFactors' (n `div` 2) divisor
+    | even n                     = 2 : primeFactors' (n `div` 2) divisor
     | divisor <= floor (isqrt n) = if n `rem` divisor == 0
-                             then divisor : primeFactors' (n `div` divisor) divisor
-                          else primeFactors' n (divisor + 2)
-    | n > 2             = [n]
-    | n <= 2             = []
+                                     then divisor : primeFactors' (n `div` divisor) divisor
+                                   else primeFactors' n (divisor + 2)
+    | n > 2                      = [n]
+    | n <= 2                     = []
 
 primeFactors :: (Integral a) => a -> [a]
 primeFactors n = primeFactors' n 3
